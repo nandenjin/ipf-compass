8a706a778223ef83535065aef10dadbb69586a2b
Kazumi Inada <hello@nandenjin.com>
2023-07-31 11:51:14 +0900

    chore: Remove debug statement

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
-    console.log(config.externals)
     config.externals.push({ knex: 'commonjs knex' })
     return config
   },
