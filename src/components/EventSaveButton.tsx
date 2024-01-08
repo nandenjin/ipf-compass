af4ea56b1207f88d8aa0be9d45e5afd0230b7da5
Kazumi Inada <hello@nandenjin.com>
2024-01-08 17:45:49 +0900

    fix: Handle save action when user logged out Fix: #38

--- src/components/EventSaveButton.tsx
+++ src/components/EventSaveButton.tsx
@@ -4,7 +4,18 @@ import { Event } from '@/lib/event'
 import { AppDispatch, RootState } from '@/store'
 import { addEvent, deleteEvent } from '@/store/collection'
 import { Button } from '@chakra-ui/button'
-import { VStack, Text } from '@chakra-ui/layout'
+import { VStack, Text, Box } from '@chakra-ui/layout'
+import {
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverFooter,
+  PopoverHeader,
+  PopoverTrigger,
+} from '@chakra-ui/popover'
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
 import { MdDone, MdStar } from 'react-icons/md'
 import { useDispatch, useSelector } from 'react-redux'
 
@@ -19,28 +30,60 @@ export function EventSaveButton({ event }: EventSaveButtonProps) {
   const dispatch = useDispatch<AppDispatch>()
 
   return (
-    <Button
-      h="100%"
-      w="6em"
-      disabled={!isLoggedIn}
-      onClick={() =>
-        dispatch(!isCollected ? addEvent(event.id) : deleteEvent(event.id))
-      }
-      bg={!isCollected ? 'gray.100' : 'red.500'}
-      _hover={{ bg: !isCollected ? 'gray.200' : 'red.600' }}
-      color={!isCollected ? 'black' : 'white'}
-    >
-      {isCollected ? (
-        <VStack>
-          <MdDone />
-          <Text>保存済み</Text>
-        </VStack>
-      ) : (
-        <VStack>
-          <MdStar />
-          <Text>保存</Text>
-        </VStack>
-      )}
-    </Button>
+    <Popover isOpen={isLoggedIn ? false : undefined}>
+      <PopoverTrigger>
+        <Button
+          h="100%"
+          w="6em"
+          onClick={() =>
+            isLoggedIn
+              ? dispatch(
+                  !isCollected ? addEvent(event.id) : deleteEvent(event.id)
+                )
+              : null
+          }
+          bg={!isCollected ? 'gray.100' : 'red.500'}
+          _hover={{ bg: !isCollected ? 'gray.200' : 'red.600' }}
+          color={!isCollected ? 'black' : 'white'}
+        >
+          {isCollected ? (
+            <VStack>
+              <MdDone />
+              <Text>保存済み</Text>
+            </VStack>
+          ) : (
+            <VStack>
+              <MdStar />
+              <Text>保存</Text>
+            </VStack>
+          )}
+        </Button>
+      </PopoverTrigger>
+      <Box position="relative" zIndex="999">
+        <PopoverContent>
+          <PopoverArrow />
+          <PopoverCloseButton />
+          <PopoverHeader fontWeight={'bold'}>
+            ログインして利用しよう
+          </PopoverHeader>
+          <PopoverBody>
+            公演を保存して、自分のスケジュールを作ることができます
+          </PopoverBody>
+          <PopoverFooter>
+            <Button
+              w="100%"
+              bg="red.500"
+              color="white"
+              onClick={() => {
+                const auth = getAuth()
+                signInWithPopup(auth, new GoogleAuthProvider())
+              }}
+            >
+              登録・ログイン
+            </Button>
+          </PopoverFooter>
+        </PopoverContent>
+      </Box>
+    </Popover>
   )
 }
